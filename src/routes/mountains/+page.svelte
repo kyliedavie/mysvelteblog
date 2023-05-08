312ff2e6c4f6d48948e7df12841bb7924f7f8f5d
kyliedavie <112646984+kyliedavie@users.noreply.github.com>
2023-05-08 16:27:44 -0400

    final commits

--- src/routes/mountains/+page.svelte
+++ src/routes/mountains/+page.svelte
@@ -5,32 +5,15 @@
     const {artworks} = data;
     console.log(artworks);
 </script>
-<!--   
-<h2>Links to Artworks as JavaScript Objects</h2> -->
-
-<!-- <article>
-    {#each artworks as { title, artist_title, image_id, date_display, medium_display }}
-    <figure>
-      <img src="https://www.artic.edu/iiif/2/{image_id}/full/843,/0/default.jpg" alt="">
-      <figcaption><strong>{title}</strong> by {artist_title}<br>{date_display} | {medium_display}</figcaption>
-    </figure>
-    {/each}
-</article> -->
 
 <article>
     {#each artworks as { title, api_link, image_id, artist_title, date_display, medium_display}}
     <figure>
       <img src="https://www.artic.edu/iiif/2/{image_id}/full/843,/0/default.jpg" alt="">
-      <!-- <a href="{api_link}">{title}</a> -->
       <figcaption><strong><a href="{api_link}">{title}</a></strong> by {artist_title}<br>{date_display} | {medium_display}</figcaption>
     </figure>
-    <!-- add image tag here, use image_id and template
-    sample id: 6b1edb9c-0f3f-0ee3-47c7-ca25c39ee360
-    url: https://www.artic.edu/iiif/2 + image_id + /full/843,/0/default.jpg -->
     {/each}
 </article>
-
-  
   
 <style>
     article {
@@ -46,29 +29,5 @@
         text-decoration: none;
     }
 </style>
-
-<!-- <script>
-    export async function load({ fetch }) {
-      const url = 'https://api.artic.edu/api/v1/artworks?fields=image_id,title,artist_title,date_display,medium_display&page=1&limit=1';
-      const artReq = await fetch(url);
-      const artRes = await artReq.json();
-      const artworks = artRes.data;
-      return {
-        props: {
-          artwork: artworks[0]
-        }
-      };
-    }
-  </script>
-
-<template>
-    <div>
-      <h1>{artwork.title}</h1>
-      <img src={`https://www.artic.edu/iiif/2/${artwork.image_id}/full/843,/0/default.jpg`} alt={artwork.title}>
-      <p>{artwork.artist_title}</p>
-      <p>{artwork.date_display}</p>
-      <p>{artwork.medium_display}</p>
-    </div>
-  </template> -->
   
   
